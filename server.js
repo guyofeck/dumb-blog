a149670588697f918976e2f5308b70df67ccc751
guyofeck <guyofeck@gmail.com>
2026-08-25 21:22:20 +0300

    Fix logo text in server.js

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ function page(title, body, user) {
 </head>
 <body>
 <header>
-  <a class="logo" href="/">🗞️ Yet Another Dumb Blog!!!</a>
+  <a class="logo" href="/">🗞️ Yet Another Dumb Blog!##</a>
   <nav>
     ${user
       ? `<span class="who">signed in as <b>${esc(user)}</b></span>
